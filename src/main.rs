fab494229cb1adea5f798defe6a3d49e43cd5bee
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-09 23:34:54 -0700

    feat: simple st & get of downloaded mini-redis companion

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // open connection to mini-redis address
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // set key: "hello" with value: "world"
+    client.set("hello", "world".into())
+          .await?;
+
+    // get key: "hello"
+    let result = client.get("hello")
+                       .await?;
+
+    println!("value from server: result:{:?}", result);
+
+    Ok(())
+}
