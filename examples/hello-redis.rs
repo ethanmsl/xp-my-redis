043757928ca57dc08f2bffc22e12225cd1dbdb36
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-11 16:19:14 -0700

    Feat&Refactor: server handling two commands; moved boilerplate to lib.rs

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -1,10 +1,12 @@
 use mini_redis::{client, Result};
+use my_redis::boilerplate;
 
 use crate::boilerplate::tracing_subscribe_boilerplate;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscribe_boilerplate("debug");
+    boilerplate::tracing_subscribe_boilerplate("debug");
+
     // open connection to mini-redis address
     let mut client = client::connect("127.0.0.1:6379").await?;
 
@@ -19,31 +21,4 @@ async fn main() -> Result<()> {
     println!("value from server: result:{:?}", result);
 
     Ok(())
-}
-
-mod boilerplate {
-    use tracing_subscriber::EnvFilter;
-
-    /// Start boilerplate tracing subscriber, with a default log level, when none is specified
-    ///
-    /// # Note:
-    /// - Not optimal.  But has more info than default and allows easily setting a default log level.
-    /// - We should return a `Result`, but I don't want to mess with the flow of this repo's tour by adding different error types and consequent handling strategies.
-    pub fn tracing_subscribe_boilerplate(default: &str) {
-        // region:    --- tracing_subscriber
-        let filter = EnvFilter::try_new(std::env::var("RUST_LOG").unwrap_or_else(|_| {
-                                                                     default.to_string()
-                                                                 })).expect("Valid filter input provided.");
-
-        tracing_subscriber::fmt().with_env_filter(filter)
-                                 .with_file(true)
-                                 .with_line_number(true)
-                                 .with_thread_ids(true)
-                                 .with_target(true)
-                                 .init();
-        // console_subscriber::init();
-        // endregion: --- tracing_subscriber
-
-        // Ok(())
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use mini_redis::Frame;
+use my_redis::boilerplate;
 use tokio::net::{TcpListener, TcpStream};
+use tracing;
 
 #[tokio::main]
 async fn main() {
+    boilerplate::tracing_subscribe_boilerplate("debug");
+
     // bind "listener" to an address
     let listener = TcpListener::bind("127.0.0.1:6379").await
                                                       .expect("Listener binds.");
@@ -18,19 +22,41 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use std::collections::HashMap;
+
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut db = HashMap::new();
     // "mini_redis specific" Read&Write "frames" instead of working with byte streams
     let mut connection = mini_redis::Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame()
-                                   .await
-                                   .expect("Frame read.")
+    while let Some(frame) = connection.read_frame()
+                                      .await
+                                      .expect("frame read")
     {
-        println!("GOT: {:?}", frame);
-
-        // Respond withn an error  <-- similar to `todo!();` without crashing program
-        let response = Frame::Error("unimplemented".to_string());
+        tracing::info!("GOT: {:?}", frame);
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // value stored as Vec<u8>
+                db.insert(cmd.key()
+                             .to_string(),
+                          cmd.value()
+                             .to_vec());
+                Frame::Simple("OK".to_string())
+            },
+            Get(cmd) =>
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone()
+                                     .into())
+                }
+                else {
+                    Frame::Null
+                },
+            cmd => unimplemented!("{:?}", cmd),
+        };
+        // write response to client
         connection.write_frame(&response)
                   .await
-                  .expect("Frame write.");
+                  .expect("Write to client");
     }
 }
