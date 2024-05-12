da103a134c375926ca55edcbbbe7d6c03bb94769
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-12 15:52:45 -0700

    Feat: working echo server
    Simple listens.  can use netcat (nc) to test

--- justfile
+++ justfile
@@ -12,3 +12,15 @@ serve LOG_LEVEL='debug':
 # Run the 'hello-redis' example, writing and requesting a key:value pair.  (Wants a Server to talk to.)
 hi LOG_LEVEL='debug':
         RUST_LOG={{LOG_LEVEL}} cargo run --example hello-redis
+
+# Run simple client set & get.
+client LOG_LEVEL='debug':
+        RUST_LOG={{LOG_LEVEL}} cargo run --bin client
+
+# Run echo server. (Listens for raw bytestreams by TCP and returns them.)
+echo-serv LOG_LEVEL='debug':
+        RUST_LOG={{LOG_LEVEL}} cargo run --bin echo-server-copy
+
+# Send raw bytes (by TCP) to echo server.
+yell *WORDS:
+         echo "{{WORDS}}" | nc 127.0.0.1 6142
--- src/bin/echo-server-copy.rs
+++ src/bin/echo-server-copy.rs
@@ -2,38 +2,28 @@
 
 use my_redis::boilerplate::tracing_subscribe_boilerplate;
 use my_redis::boilerplate::SubKind;
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
+use tokio::{io, net::TcpListener};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
         tracing_subscribe_boilerplate(SubKind::Tracing(String::from("debug")));
         // tracing_subscribe_boilerplate(SubKind::Console);
         tracing::info!("Tracing Subscriber active.");
-        tracing::debug!("Attempting to connect to local port...");
-        // Stream that is R&&W. We split, so we can mutably borrow a separate component for each.
-        let socket = TcpStream::connect("127.0.0.1:6142").await?;
-        let (mut rd, mut wr) = io::split(socket);
+        tracing::warn!(
+                r#"This server accepts TCP, not HTTP.  If you'd like to hear something echoed back try:\n`echo "Hello, echo server" | nc 127.0.0.1 6142`"#
+        );
+        tracing::debug!("Attempting to listen to local port...");
+        // Note: this is a different port than for reddis comms
+        let listener = TcpListener::bind("127.0.0.1:6142").await?;
 
-        tracing::debug!("Spawning Socket writer...");
-        // separate thread writing
-        tokio::spawn(async move {
-                wr.write_all(b"hello\r\n").await?;
-                wr.write_all(b"world\r\n").await?;
-                Ok::<_, io::Error>(()) // TODO: what are mechanisms and limits of type inference?
-        });
-
-        tracing::debug!("Reading from socket...");
-        let mut buf = vec![0; 128];
         loop {
-                let n = rd.read(&mut buf).await?;
-                if n == 0 {
-                        break;
-                }
-                tracing::info!(
-                        "GOT: {:?}",
-                        std::str::from_utf8(&buf[..n]).expect("valid utf8")
-                );
+                let (mut socket, _) = listener.accept().await?;
+                tokio::spawn(async move {
+                        let (mut rd, mut wr) = socket.split();
+                        if io::copy(&mut rd, &mut wr).await.is_err() {
+                                eprintln!("failed to copy");
+                        }
+                });
         }
-        Ok(())
+        // unreachable!();
 }
