920cb4f5b7896a2b4edc3cbce3c600e8e82dd68b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-23 17:55:36 -0400

    Future: mini tokio, sans wakers

--- examples/future.rs
+++ examples/future.rs
@@ -1,10 +1,12 @@
 //! Example future implementation
 
+use futures::task;
 use my_redis::boilerplate;
 use my_redis::error::Result;
 use tokio::net::TcpStream;
 use tracing;
 
+use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
@@ -56,6 +58,39 @@ async fn my_async_fn() -> Result<()> {
     tracing::info!("async TCP operation complete");
 
     Ok(())
+}
+// //////////////////////////////////////////////
+
+struct MiniTokio {
+    tasks: VecDeque<Task>,
+}
+
+type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+impl MiniTokio {
+    fn new() -> MiniTokio {
+        MiniTokio {
+            tasks: VecDeque::new(),
+        }
+    }
+
+    /// Spawn a future onto the mini-tokio instance.
+    fn spawn<F>(&mut self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        self.tasks.push_back(Box::pin(future));
+    }
+
+    fn run(&mut self) {
+        let waker = task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+        while let Some(mut task) = self.tasks.pop_front() {
+            if task.as_mut().poll(&mut cx).is_pending() {
+                self.tasks.push_back(task);
+            }
+        }
+    }
 }
 
 // ///////////////////////////////////////////////
