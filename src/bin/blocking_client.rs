08bc449a0ef246619380da630aba5a0f27e5bcde
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-25 23:19:16 -0400

    Chore: abandoned blocking client code

---
+++ src/bin/blocking_client.rs
@@ -0,0 +1,29 @@
+// //! Blocking interface to async code
+
+// use tokio::net::ToSocketAddrs;
+// use tokio::runtime::Runtime;
+
+// use mini_redis::client::Message;
+
+// /// Async client and it's runtime
+// struct BlockingClient {
+//         inner: mini_redis::client::Client,
+//         rt: Runtime,
+// }
+
+// impl BlockingClient {
+//         fn connect<T: ToSocketAddrs>(addr: T) -> tokio::io::result::Runtime<Runtime> {
+//                 let rt = tokio::runtime::Builder::new_current_thread()
+//                         .enable_all()
+//                         .build();
+//                 let inner = rt.block_on(mini_redis::client::Client::connect(addr));
+//         }
+// }
+
+// fn main() {
+//         println!("hello");
+// }
+
+fn main() {
+        unreachable!("mess of unresolvable references -- guide doesn't make sensible suggestions and I don't want to hunt through mini-redis")
+}
