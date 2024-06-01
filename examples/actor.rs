7af47c9c68855e808bbd2ccd8e8b7bf91038fdc5
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-01 02:40:23 -0400

    Feat(example): actor definition with tokio

---
+++ examples/actor.rs
@@ -0,0 +1,96 @@
+//! actor example using tokio
+
+use my_redis::boilerplate::{tracing_subscribe_boilerplate, SubKind};
+use tokio::{io::{self, AsyncReadExt, AsyncWriteExt},
+            net::TcpStream,
+            sync::{mpsc, oneshot}};
+
+/// Receiver component to listen in on
+#[derive(Debug)]
+struct MyActor {
+    receiver:   mpsc::Receiver<ActorMessage>,
+    actor_info: Option<u32>,
+}
+
+/// Message to send to an actor
+///
+/// Should contaain message *for* actor
+/// and sending channel for actor to *respond* with
+/// Generator should hold on to receiver end, ofc.
+#[derive(Debug)]
+enum ActorMessage {
+    SendMessage {
+        message:    String,
+        respond_to: oneshot::Sender<Option<u32>>,
+    },
+}
+
+impl MyActor {
+    fn new(receiver: mpsc::Receiver<ActorMessage>) -> Self {
+        MyActor { receiver,
+                  actor_info: Some(0) }
+    }
+
+    async fn handle_message(&mut self, msg: ActorMessage) -> io::Result<()> {
+        match msg {
+            ActorMessage::SendMessage { message,
+                                        respond_to, } => {
+                match message {
+                    _ if message.contains("increase") => {
+                        if let Some(info) = self.actor_info.as_mut() {
+                            *info += 1;
+                        }
+                        let _ = respond_to.send(None);
+                    }
+                    _ if message.contains("get") => {
+                        let _ = respond_to.send(self.actor_info);
+                    }
+                    _ => {
+                        println!("Unknown message: {}", message);
+                        let _ = respond_to.send(None);
+                    }
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
+// //////////////////////////////////// //
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    tracing_subscribe_boilerplate(SubKind::Tracing(String::from("debug")));
+
+    let (tx, rx) = mpsc::channel::<ActorMessage>(32); // number is buffer size
+    let mut yorrick = MyActor::new(rx);
+
+    // use tokio spawn to run MyActor and have it wait to receive messages
+    // we don't take a yorrick_handle here; using our mpsc & oneshots soley
+    let _ = tokio::spawn(async move {
+        while let Some(msg) = yorrick.receiver.recv().await {
+            tracing::info!("Received message: {:?}", msg);
+            yorrick.handle_message(msg)
+                   .await
+                   .expect("Message handling failed.");
+        }
+        tracing::warn!("Actor has stopped receiving messages.");
+    });
+
+    // oneshot channel to receive response from actor
+    let (txo, rxo) = oneshot::channel();
+    let msg = ActorMessage::SendMessage { message:    String::from("increase"),
+                                          respond_to: txo, };
+
+    // send message to actor
+    tracing::info!("Sending message to actor.");
+    tx.send(msg).await.expect("Message sent.");
+    tracing::info!("Message sent to actor succesfully.");
+
+    // wait for oneshot response
+    tracing::debug!("Waiting for response from actor.");
+    let response = rxo.await.expect("Response received.");
+    println!("Response: {:?}", response);
+
+    println!("unimplemented");
+    Ok(())
+}
